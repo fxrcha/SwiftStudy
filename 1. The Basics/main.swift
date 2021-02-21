42d42dadd2c5978666e3f41b657b66376781a62a
fxrcha <truetype24@gmail.com>
2021-02-22 04:07:01 +0900

    The Basics 40%

---
+++ 1. The Basics/main.swift
@@ -0,0 +1,25 @@
+// Declare a variable with let and var keyword.
+
+var fxrchaisStupid = 1
+let fxrchaisGenius = 0
+
+// This means declare a constant named "fxrchaisGenius", and give it a integer value 0.
+// Also, this means declar a variable namemd "fxrchaisStupid", and give it a integer value 1.
+
+var annotationTest: String
+
+// This means declare a variable called "annotationTest", of type String
+// This variable only can store String values.
+
+annotationTest = "Hello World!" // Success
+/* annotationTest = 1 will give error */
+
+var paloalto, cjamm, bewhy: String // Also can annotate multiple variables
+
+print(fxrchaisStupid) // Prints 1
+print("The value of a string is \(annotationTest)") // Something like f-string
+
+let hungry = "I am"; print(hungry)
+
+// Swift doesn't require a semicolon, but it will be required when you write several statetments.
+
